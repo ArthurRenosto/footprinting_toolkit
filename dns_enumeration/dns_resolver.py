8255ac5611d035d78f7c375d8ed193b9b508bc0d
ArthurRenosto <arthurrenosto123@gmail.com>
2025-08-19 10:11:17 -0300

    dns_resolver corrigido

--- dns_enumeration/dns_resolver.py
+++ dns_enumeration/dns_resolver.py
@@ -1,9 +1,9 @@
-from dns_basic import resolver as dns
+from dns import resolver as dns
 
 dominio = input("Digite o dominio alvo: ")
 a = ["MX", "NS"]
 
 for registro in a:
-    resultado = dns.resolver.query(dominio, registro, raise_on_no_answer=False) # resolver.query() usado para determinar se determinado dominio existe ou nao
+    resultado = dns.resolve(dominio, registro, raise_on_no_answer=False) # resolver.query() usado para determinar se determinado dominio existe ou nao
     if resultado.rrset is not None:
         print(resultado.rrset) # o .rrset é usado para ele nao trazer o hexadecimal(endereço da memoria), e sim o valor que está neste endereço
